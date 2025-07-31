72d9a5a60f51b9f6a01edc2062f60603376c1871
mohameddefalaa <mohamedismaildefalaa@gmail.com>
2025-07-31 21:00:26 +0300

    Initial commit: create Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: Splash());
+  }
+}
