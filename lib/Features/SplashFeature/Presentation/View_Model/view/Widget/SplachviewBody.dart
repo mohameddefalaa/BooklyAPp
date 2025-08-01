c3ea7b81a90af2e53ab7224324330ad42fb90e3b
mohameddefalaa <mohamedismaildefalaa@gmail.com>
2025-08-01 16:12:47 +0300

    refactore init state & navigate to home view

--- lib/Features/SplashFeature/Presentation/View_Model/view/Widget/SplachviewBody.dart
+++ lib/Features/SplashFeature/Presentation/View_Model/view/Widget/SplachviewBody.dart
@@ -1,7 +1,10 @@
+import 'package:bookly/Features/Home/Presentation/VIews/Home_view.dart';
 import 'package:bookly/Features/SplashFeature/Presentation/View_Model/view/Widget/slidingtext.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/utils.dart';
 
 class SpalshViewBody extends StatefulWidget {
   const SpalshViewBody({super.key});
@@ -17,16 +20,18 @@ class _SpalshViewBodyState extends State<SpalshViewBody>
   @override
   void initState() {
     super.initState();
+    initSlidingAnimiton();
+    navigateHomeView();
+  }
 
-    animationcontroller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-    slidinganimation = Tween<Offset>(
-      begin: Offset(-1, 0),
-      end: Offset.zero,
-    ).animate(animationcontroller);
-    animationcontroller.forward();
+  void navigateHomeView() {
+    Future.delayed(Duration(seconds: 2), () {
+      Get.to(
+        HomeView(),
+        transition: Transition.leftToRight,
+        duration: Duration(milliseconds: 250),
+      );
+    });
   }
 
   @override
@@ -52,5 +57,17 @@ class _SpalshViewBodyState extends State<SpalshViewBody>
         SlidingText(slidinganimation: slidinganimation),
       ],
     );
+  }
+
+  void initSlidingAnimiton() {
+    animationcontroller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    slidinganimation = Tween<Offset>(
+      begin: Offset(-1, 0),
+      end: Offset.zero,
+    ).animate(animationcontroller);
+    animationcontroller.forward();
   }
 }
